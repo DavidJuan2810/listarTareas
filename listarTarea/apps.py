fbbca7c642988804df869fcf0e9a747230314a50
DavidJuan2810 <Juandavides120@gmail.com>
2024-08-23 08:43:48 -0500

    Creacion de proyecto

---
+++ listarTarea/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ListartareaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'listarTarea'
---
+++ listarTarea/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Tarea(models.Model):
+    ESTADO_CHOISES=[
+        ('completado','Completado'),
+        ('pendiente','Pendiente')
+    ]
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+    completado = models.CharField(max_length=100, choices= ESTADO_CHOISES )
+
+    def __str__(self):
+        return self.titulo
+
